b7313c42bbe39613c688545f2e249b5c6442654e
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-06-24 21:07:58 -0300

    Getting mock data in ItemListContainer through a promise and setting items to deliver to ItemList in a useEffect Hook

--- src/components/itemListContainer/itemListContainer.js
+++ src/components/itemListContainer/itemListContainer.js
@@ -1,6 +1,9 @@
 import './itemListContainer.scss';
+import { useEffect, useState } from 'react';
 import { ItemCount } from '../itemCount/itemCount';
+import { ItemList } from '../itemList/itemList';
 import { WORDINGS } from '../../wordings';
+import MOCK_DATA from '../../assets/MOCK_DATA.json';
 
 export const ItemListContainer = (props) => {
     const { greeting } = props;
@@ -15,12 +18,27 @@ export const ItemListContainer = (props) => {
         itemStock: 0
     }
 
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const getItems = new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(MOCK_DATA)
+            }, 2000)
+        })
+    
+        getItems.then(items => setItems(items))
+    }, [])
+
     return (
         <>
             <h1 className="landing-title">{greeting}</h1>
             <h3 className="landing-subtitle">{WORDINGS.ITEMS_EXAMPLE}</h3>
-            <ItemCount {...withStockPops}/>
-            <ItemCount {...withoutStockPops}/>
+            <div className="item-count-example">
+                <ItemCount {...withStockPops}/>
+                <ItemCount {...withoutStockPops}/>
+            </div>
+            <ItemList items={items} />
         </>
     )
 }
