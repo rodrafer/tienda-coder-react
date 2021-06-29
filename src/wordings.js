c66f89a72d53ed7549aa416a3005bd7bf034d8b2
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-06-29 00:53:09 -0300

    Adding new wordings

--- src/wordings.js
+++ src/wordings.js
@@ -1,7 +1,7 @@
 export const WORDINGS = {
     // TITLES
     LANDING_TITLE: '¡Bienvenido a Reactive Games!',
-    ITEMS_EXAMPLE: 'Aquí puedes ver algunos contadores de ítems y una lista de ejemplo:',
+    ITEMS_EXAMPLE: 'Aquí puedes ver algunos ítems y un detalle de ejemplo:',
 
     // BUTTONS
     ADD_TO_CART: 'Agregar al carrito',
@@ -13,5 +13,6 @@ export const WORDINGS = {
     // FEEDBACKS
     ONE_ITEM_ADDED: 'fue añadido al carrito exitosamente',
     SEVERAL_ITEMS_ADDED: 'fueron añadidos al carrito exitosamente',
-    SEARCHING_FOR_PRODUCTS: 'Buscando los mejores productos...'
+    SEARCHING_FOR_PRODUCTS: 'Buscando los mejores productos...',
+    LOADING_PRODUCT_DETAIL: 'Cargando detalle del producto...'
 }
