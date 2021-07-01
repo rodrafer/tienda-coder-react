f400badac05f15c77c44f81e07816b083b8882d9
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-01 00:29:12 -0300

    Creating CategoryPage component to wrap ItemListContainer

---
+++ src/pages/categoryPage/categoryPage.js
@@ -0,0 +1,10 @@
+import './categoryPage.scss';
+import { ItemListContainer } from '../../components/itemListContainer/itemListContainer';
+
+export const CategoryPage = () => {
+    return (
+        <div className="category-page">
+            <ItemListContainer />
+        </div>
+    )
+}
