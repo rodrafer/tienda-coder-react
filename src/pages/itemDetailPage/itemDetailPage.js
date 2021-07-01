bf6b2ad69e4e315e6e4dbeb0188b312049902941
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-01 00:29:36 -0300

    Creating ItemDetailPage to wrap ItemDetailContainer

---
+++ src/pages/itemDetailPage/itemDetailPage.js
@@ -0,0 +1,10 @@
+import './itemDetailPage.scss';
+import { ItemDetailContainer } from '../../components/itemDetailContainer/itemDetailContainer';
+
+export const ItemDetailPage = () => {
+    return (
+        <div className="item-detail-page">
+            <ItemDetailContainer />
+        </div>
+    )
+}
