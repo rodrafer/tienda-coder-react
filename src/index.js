8019555f1662fc4b4f53b1a93164780c100dd7c1
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-01 22:16:48 -0300

    Including CartProvider in index.js to turn App component into a provider for the entire app

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { CartProvider } from './context/cartContext';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <CartProvider>
+      <App />
+    </CartProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
