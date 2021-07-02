f7d4d9e3684523b00ae6c8f9053b1a6d32105961
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-01 23:57:25 -0300

    Including url param in itemDetailPage and passing it to ItemDetailContainer

--- src/pages/itemDetailPage/itemDetailPage.js
+++ src/pages/itemDetailPage/itemDetailPage.js
@@ -1,10 +1,13 @@
 import './itemDetailPage.scss';
+import { useParams } from 'react-router';
 import { ItemDetailContainer } from '../../components/itemDetailContainer/itemDetailContainer';
 
 export const ItemDetailPage = () => {
+    const { itemId } = useParams();
+
     return (
         <div className="item-detail-page">
-            <ItemDetailContainer />
+            <ItemDetailContainer itemId={itemId} />
         </div>
     )
 }
