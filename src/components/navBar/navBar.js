9623489c2c99981259935b36e5e9c69ab60e500f
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-01 23:57:56 -0300

    Fixing probems in navBar

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,23 +1,29 @@
 import './navBar.scss';
-import { snakeCase } from "snake-case";
-import { NavLink } from 'react-router-dom';
+import { snakeCase } from 'snake-case';
+import { v4 as uuidv4 } from 'uuid';
+import { Link, NavLink } from 'react-router-dom';
 import logo from '../../assets/logo-with-background.svg';
 import { CartWidget } from '../cartWidget/cartWidget';
 import { WORDINGS } from '../../wordings';
 
 export const NavBar = () => {
     const renderCategories = () => {
-        return WORDINGS.CATEGORIES.map(category => 
-            <li key={category} className="category">
-                <NavLink to={`category/${snakeCase(category)}`} activeClassName="category__link">{category}</NavLink>
-            </li>
-        )
+        return WORDINGS.CATEGORIES.map(category => {
+            const keyId = uuidv4().slice(0, 4);
+            return (
+                <li key={keyId} className="category">
+                    <NavLink to={`/category/${snakeCase(category)}`} activeClassName="category__link">{category}</NavLink>
+                </li>
+            )
+        })
     }
 
     return (
         <>
             <div className="wrapper-left">
-                <img src={logo} alt="logo" className="logo"></img>
+                <Link to="/">
+                    <img src={logo} alt="logo" className="logo"></img>
+                </Link>
                 <nav className="menu">
                     <ul className="menu__list">
                         {renderCategories()}
