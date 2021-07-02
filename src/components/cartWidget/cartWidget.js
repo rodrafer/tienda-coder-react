118607eabcf433ff6f909aa1df31df3393135238
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-01 22:18:24 -0300

    Consuming CartContext in CartWidget component through a useContext hook

--- src/components/cartWidget/cartWidget.js
+++ src/components/cartWidget/cartWidget.js
@@ -1,8 +1,20 @@
 import './cartWidget.scss';
 import shoppingCart from '../../assets/shopping-trolley.png';
+import { useContext } from 'react';
+import { CartContext } from '../../context/cartContext';
 
 export const CartWidget = () => {
+    const { cart, setItem } = useContext(CartContext);
+
+    if (!cart.includes('item0')) {
+        setItem('item0')
+    }
+    console.log(cart)
+
     return (
-        <img src={shoppingCart} alt="cart" className="shopping-cart"></img>
+        <>
+            <img src={shoppingCart} alt="cart" className="shopping-cart"></img>
+            <div style={{color: 'white'}}>{cart[0]}</div>
+        </>
     )
 }
