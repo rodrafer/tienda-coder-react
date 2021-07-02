5c10707dd6db6c8f8812fc4f68ad2e2ea91e94aa
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-01 23:57:41 -0300

    Passing url param to ItemListContainer

--- src/pages/categoryPage/categoryPage.js
+++ src/pages/categoryPage/categoryPage.js
@@ -4,10 +4,10 @@ import { useParams } from 'react-router';
 
 export const CategoryPage = () => {
     const { categoryId } = useParams();
-    
+
     return (
         <div className="category-page">
-            <ItemListContainer />
+            <ItemListContainer categoryId={categoryId} />
         </div>
     )
 }
