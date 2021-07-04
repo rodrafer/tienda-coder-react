ed1d162f2e2fe517b374d61b72d06505de44b366
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-04 17:56:05 -0300

    Creating Cart component to show some feedback

---
+++ src/components/cart/cart.js
@@ -0,0 +1,10 @@
+import './cart.scss';
+import { WORDINGS } from '../../wordings';
+
+export const Cart = () => {
+    return (
+        <div className="cart">
+            <h1>{WORDINGS.CART_COMMING_SOON}</h1>
+        </div>
+    )
+}
