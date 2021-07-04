1d6c0f9fc30c3dc001881c6c7b28a624b4684249
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-04 17:49:19 -0300

    Taking out ItemCount and subtitle from ItemListContainer

--- src/components/itemListContainer/itemListContainer.js
+++ src/components/itemListContainer/itemListContainer.js
@@ -1,32 +1,11 @@
 import './itemListContainer.scss';
 import { useEffect, useState } from 'react';
 import { snakeCase } from 'snake-case';
-import { ItemCount } from '../itemCount/itemCount';
 import { ItemList } from '../itemList/itemList';
-import { WORDINGS } from '../../wordings';
 import MOCK_DATA from '../../assets/MOCK_DATA.json';
 
 export const ItemListContainer = (props) => {
     const { greeting, categoryId } = props;
-
-    const onAdd = (stock, itemName, count) => {
-        const addedItemsText = count === 1 ? WORDINGS.ONE_ITEM_ADDED : WORDINGS.SEVERAL_ITEMS_ADDED;
-        stock && alert(`¡Felicitaciones! ${count} ${itemName} ${addedItemsText}. (Respuesta simulada)`);
-    }
-
-    const withStockPops = {
-        itemName: 'Nintendo Switch',
-        stock: 6,
-        initial: 3,
-        onAdd: onAdd
-    }
-
-    const withoutStockPops = {
-        itemName: 'Play Station 5',
-        stock: 0,
-        initial: 0,
-        onAdd: onAdd
-    }
 
     const [items, setItems] = useState([]);
     const [hasLoaded, setHasLoaded] = useState(false);
@@ -42,16 +21,13 @@ export const ItemListContainer = (props) => {
             setItems(items)
             setHasLoaded(true)
         })
+
+        return setHasLoaded(false)
     }, [categoryId])
 
     return (
         <>
-            <h1 className="landing-title">{greeting}</h1>
-            <h3 className="landing-subtitle">{WORDINGS.ITEMS_EXAMPLE}</h3>
-            <div className="item-count-example">
-                <ItemCount {...withStockPops} />
-                <ItemCount {...withoutStockPops} />
-            </div>
+            {greeting && <h1 className="landing-title">{greeting}</h1>}
             <ItemList items={items} hasLoaded={hasLoaded} />
         </>
     )
