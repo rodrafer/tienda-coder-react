ed8d8c810c1ec62a696ac6f602866095585e4fca
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-11 20:46:55 -0300

    Showing message and back to home button if cart is empty

--- src/components/cart/cart.js
+++ src/components/cart/cart.js
@@ -1,6 +1,7 @@
 import './cart.scss';
 import { v4 as uuidv4 } from 'uuid';
 import { useContext } from 'react';
+import { Link } from 'react-router-dom';
 import { CartContext } from '../../context/cartContext';
 import { ItemCount } from '../itemCount/itemCount';
 
@@ -24,17 +25,30 @@ export const Cart = () => {
                     <h4 className="cart-list__item-title">{item.title}</h4>
                     <p className="cart-list__item-price">{item.price}</p>
                     <ItemCount {...itemCountProps} />
-                    <button className="cart-list__item-remove" onClick={() => removeItem(item.id)}>X</button>
+                    <button className="cart-list__item-remove command-button" onClick={() => removeItem(item.id)}>X</button>
                 </li>
             )
         })
     }
 
+    const renderEmptyCartAction = () => {
+        return (
+            <div className="cart-empty">
+                <h4>¡Tu carrito está vacío!</h4>
+                <p>Volvé a la página principal para agregar increíbles productos</p>
+                <Link to="/" className="cart-empty__back-to-home main-button">Volver al inicio</Link>
+            </div>
+        )
+    }
+
     return (
         <div className="cart">
-            <ul className="cart-list">
-                {renderItemsInCart()}
-            </ul>
+            <h1>Tu carrito</h1>
+            {cart.length
+                ? <ul className="cart-list">
+                    {renderItemsInCart()}
+                </ul>
+                : renderEmptyCartAction()}
         </div>
     )
 }
