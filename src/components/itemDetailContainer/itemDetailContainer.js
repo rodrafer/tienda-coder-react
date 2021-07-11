b1c1be5434743a237489d4a2d7d2a0d96004a9b9
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-11 16:38:11 -0300

    Including a database with firebase in ItemDetailConainer

--- src/components/itemDetailContainer/itemDetailContainer.js
+++ src/components/itemDetailContainer/itemDetailContainer.js
@@ -1,5 +1,6 @@
 import './itemDetailContainer.scss';
 import { useEffect, useState } from 'react';
+import { dataBase } from '../../firebase/firebase';
 import { ItemDetail } from '../itemDetail/itemDetail';
 import { WORDINGS } from '../../wordings';
 import MOCK_DATA from '../../assets/MOCK_DATA.json';
@@ -16,7 +17,25 @@ export const ItemDetailContainer = (props) => {
         })
 
         getItem.then(item => setItemToShow(item))
-    }, [itemId])
+    }, [itemId]);
+
+    useEffect(() => {
+        const db = dataBase;
+        const itemCollection = db.collection('productos');
+        const item = itemCollection.doc(itemId);
+        item.get().then(doc => {
+            if (!doc.exists) {
+                console.log('No existe este ítem');
+                return               
+            }
+            console.log('Ítem encontrado');
+            setItemToShow({ id: doc.id, ...doc.data() }) // firebase devuelve doc con id, ref, metadata y data() separado del id
+        }).catch(error => {
+            console.log('Error al buscar ítems:', error)
+        }).finally(() => { // Sucede por más que la promise salga bien o mal (then o catch)
+            // setHasLoaded(true);
+        })
+    }, [])
 
     return (
         itemToShow
