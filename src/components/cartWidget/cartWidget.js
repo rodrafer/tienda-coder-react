4791a361f00047c9b44a2f7f281604d6e704c830
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-12 00:12:43 -0300

    Including Link in CartWidget to navigate to /cart when clicking on it

--- src/components/cartWidget/cartWidget.js
+++ src/components/cartWidget/cartWidget.js
@@ -2,14 +2,15 @@ import './cartWidget.scss';
 import shoppingCart from '../../assets/shopping-trolley.png';
 import { useContext } from 'react';
 import { CartContext } from '../../context/cartContext';
+import { Link } from 'react-router-dom';
 
 export const CartWidget = () => {
     const { totalQuantity } = useContext(CartContext);
 
     return (
-        <div className="shopping-cart">
+        <Link to="/cart" className="shopping-cart">
             <p className="shopping-cart__quantity">{totalQuantity}</p>
             <img src={shoppingCart} alt="cart" className="shopping-cart__icon"></img>
-        </div>
+        </Link>
     )
 }
