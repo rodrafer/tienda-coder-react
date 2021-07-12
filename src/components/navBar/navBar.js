3549577fead9cce8613e6805c5eee5400078240e
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-12 00:09:39 -0300

    Consuming CartContext in NavBar to verify if cart has items and render CartWidget conditionally

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,13 +1,17 @@
 import './navBar.scss';
 import { snakeCase } from 'snake-case';
 import { v4 as uuidv4 } from 'uuid';
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 import logo from '../../assets/logo-with-background.svg';
 import { CartWidget } from '../cartWidget/cartWidget';
 import { WORDINGS } from '../../wordings';
+import { CartContext } from '../../context/cartContext';
 
 export const NavBar = () => {
+    const { cart } = useContext(CartContext);
+    const shouldRenderCartWidget = !!cart.length;
+
     const renderCategories = () => {
         return WORDINGS.CATEGORIES.map(category => {
             const keyId = uuidv4().slice(0, 4);
@@ -32,7 +36,7 @@ export const NavBar = () => {
                 </nav>
             </div>
             <div className="wrapper-right">
-                <CartWidget />
+                {shouldRenderCartWidget && <CartWidget />}
             </div>
         </Fragment>
     )
