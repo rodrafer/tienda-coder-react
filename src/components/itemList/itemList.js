63743c62d21ba622e8878c9ef805e79395a03fa0
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-12 01:05:18 -0300

    Setting category pages' title to category name

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,16 +1,23 @@
 import './itemList.scss';
 import { Fragment } from 'react';
+import { snakeCase } from 'snake-case';
 import { Item } from '../item/item';
 import { WORDINGS } from '../../wordings';
 
+const replaceSpecialCharacters = require('replace-special-characters');
+
 export const ItemList = (props) => {
-    const { items, hasLoaded } = props
+    const { items, hasLoaded, categoryId } = props
+
+    const titleCategory = WORDINGS.CATEGORIES.find(category =>
+        snakeCase(replaceSpecialCharacters(category)) === categoryId
+    );
 
     const renderItemList = () => {
         if (hasLoaded) {
             return items.length
                 ? <Fragment>
-                    <h3 className="item-list__example">{WORDINGS.ITEMS_EXAMPLE}</h3>
+                    <h1 className="item-list__example">{titleCategory}</h1>
                     {items.map(item => <Item item={item} key={item.id} />)}
                 </Fragment>
                 : <h1>{WORDINGS.CONTENT_NOT_FOUND}</h1>
