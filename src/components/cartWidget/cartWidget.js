fc5482047a200c291d0332cfd1e4a274d4d20981
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-07-11 23:50:14 -0300

    Including total quantity of items in cart in CartWidget

--- src/components/cartWidget/cartWidget.js
+++ src/components/cartWidget/cartWidget.js
@@ -1,8 +1,15 @@
 import './cartWidget.scss';
 import shoppingCart from '../../assets/shopping-trolley.png';
+import { useContext } from 'react';
+import { CartContext } from '../../context/cartContext';
 
 export const CartWidget = () => {
+    const { totalQuantity } = useContext(CartContext);
+
     return (
-        <img src={shoppingCart} alt="cart" className="shopping-cart"></img>
+        <div className="shopping-cart">
+            <p className="shopping-cart__quantity">{totalQuantity}</p>
+            <img src={shoppingCart} alt="cart" className="shopping-cart__icon"></img>
+        </div>
     )
 }
