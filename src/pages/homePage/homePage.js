ddaebe269fe1c5074834cdce2b8ee7dbad11a9be
Rodrigo Fernandez Frittelli <rodrigo.frittelli@mercadolibre.com>
2021-08-08 21:10:01 -0300

    Conditionally rendering content or login box in home page

--- src/pages/homePage/homePage.js
+++ src/pages/homePage/homePage.js
@@ -1,15 +1,18 @@
 import './homePage.scss';
 import { ItemListContainer } from '../../components/itemListContainer/itemListContainer';
+import { Login } from '../../components/login/login';
 import { WORDINGS } from '../../wordings';
 
-export const HomePage = () => {
-    const childrenProps = {
-        greeting: WORDINGS.LANDING_TITLE
-    }
+export const HomePage = (loginProps) => {
+  const { user } = loginProps;
 
-    return (
-        <div className="home-page">
-            <ItemListContainer {...childrenProps} />
-        </div>
-    )
+  const listContainerProps = {
+    greeting: WORDINGS.LANDING_TITLE,
+  };
+
+  return (
+    <div className="home-page">
+      {user ? <ItemListContainer {...listContainerProps} /> : <Login {...loginProps} />}
+    </div>
+  )
 }
